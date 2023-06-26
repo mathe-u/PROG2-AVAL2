59f824f4205c56c4ac98474634fedbd074ac0e31
Matheus Silva de Sousa <matheussilvades@gmail.com>
2023-06-26 18:19:37 -0300

    Add 'music.dart' file.

---
+++ diglib/lib/music.dart
@@ -0,0 +1,15 @@
+import './media.dart';
+
+class Music extends Media {
+  final String artist;
+
+  Music(
+    super.type,
+   super.title,
+   super.duration,
+   this.artist
+   );
+
+  @override
+  String get name => artist;
+}
