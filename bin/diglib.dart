3506fbc1ab1c317abdfe8f64851e253c1a324d05
Matheus Silva de Sousa <matheussilvades@gmail.com>
2023-07-01 15:48:48 -0300

    Remove 'bin/diglib.dart' file.

--- bin/diglib.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-import 'dart:io';
-
-void main() {
-  final jsonFile = json.decode(File("./data/media.json").readAsStringSync());
-  for (var jsonItem in jsonFile) {
-    print(jsonItem["type"]);
-  }
-}
