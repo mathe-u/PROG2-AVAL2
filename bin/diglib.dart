e158915585d9faad538b32392cda4506b3cd5805
Matheus Silva de Sousa <matheussilvades@gmail.com>
2023-07-01 12:56:13 -0300

    test

--- bin/diglib.dart
+++ bin/diglib.dart
@@ -4,6 +4,6 @@ import 'dart:io';
 void main() {
   final jsonFile = json.decode(File("./data/media.json").readAsStringSync());
   for (var jsonItem in jsonFile) {
-    print("${jsonItem["type"]}\t${jsonItem["title"]}\t${jsonItem["name"]}\t${jsonItem["duration"]}");
+    print(jsonItem["type"]);
   }
 }
